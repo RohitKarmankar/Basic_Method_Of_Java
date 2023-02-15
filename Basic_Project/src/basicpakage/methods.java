50a8577cd967564098b27cd9460ad4a2594ed50f
JOHN <JOHN@DESKTOP-6AG2VUU>
2023-02-15 03:50:16 -0800

    basicMethod

---
+++ Basic_Project/src/basicpakage/methods.java
@@ -0,0 +1,12 @@
+package basicpakage;
+
+public class methods {
+
+	public static void staticmethod() {
+	System.out.println("static method");	
+	}
+	
+	public static void main(String[] args) {
+    staticmethod();	
+	}
+}
