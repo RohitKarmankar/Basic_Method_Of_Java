1dc17eee86e9e4c64af4c0f01e42d182c5a86791
JOHN <JOHN@DESKTOP-6AG2VUU>
2023-02-15 04:09:11 -0800

    non static method logic

--- Basic_Project/src/basicpakage/methods.java
+++ Basic_Project/src/basicpakage/methods.java
@@ -5,8 +5,12 @@ public class methods {
 	public static void staticmethod() {
 	System.out.println("static method");	
 	}
-	
+	public void nonstaticmethod() {
+	System.out.println("non static method");	
+	}
 	public static void main(String[] args) {
-    staticmethod();	
+    staticmethod();
+    methods kk=new methods();
+    kk.nonstaticmethod();
 	}
 }
