b78919b85e97af713088d2fac4c06e54ed335e3b
rohitkarmankar327@gmail.com <rohitkarmankar327@gmail.com>
2023-02-15 06:54:56 -0800

    message to commit the changes Basic_Method EA-202345

--- Basic_Project/src/basicpakage/methods.java
+++ Basic_Project/src/basicpakage/methods.java
@@ -1,8 +1,6 @@
 package basicpakage;
 
 public class methods {
-    int a=20;
-    int b=20;
 	public static void staticmethod() {
 	System.out.println("static method");	
 	}
@@ -12,12 +10,14 @@ public class methods {
 	public void secondnonstaticmethod() {
 	System.out.println("second non static method");	
 	}
+	public void thirdnonstaticmethod() {
+	System.out.println("third non static method");	
+	}
 	public static void main(String[] args) {
     staticmethod();
     methods kk=new methods();
     kk.nonstaticmethod();
     kk.nonstaticmethod();
-    int c=kk.a+kk.b;
-    System.out.println(c);
+    kk.thirdnonstaticmethod();
 	}
 }
